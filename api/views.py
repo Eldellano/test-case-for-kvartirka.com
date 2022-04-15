ee5219d4ff66d5b60b2537f51d99d6c3706ec975
Vladislav Kalman <Vladislav_993@mail.ru>
2022-04-16 00:23:52 +0300

    add serializer, view for post

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from . import serializers
+from .models import Post
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
+    #@csrf_exempt
+    def perform_create(self, serializer):
+        serializer.save(title=self.request.query_params['title'], text=self.request.query_params['text'])
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
